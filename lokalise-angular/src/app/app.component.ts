0dad32593e6b3134e7ae0261d20968ad65eb81e1
adriana zazueta <adriana.zazueta@uabc.edu.mx>
2025-02-03 19:50:33 -0800

    save lang file

--- lokalise-angular/src/app/app.component.ts
+++ lokalise-angular/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    template: `<h1>{{ 'HELLO' | translate }}</h1>`
 })
 export class AppComponent {
-  title = 'lokalise-angular';
+    constructor(private translate: TranslateService) {
+        this.translate.setDefaultLang('en');
+    }
 }
